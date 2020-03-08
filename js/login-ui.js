f756e5ac0c4ede4a6624b84e589241e9531f25a9
forbesashli <44144720+forbesashli@users.noreply.github.com>
2020-03-08 11:27:21 -0700

    loginbabet

--- js/login-ui.js
+++ js/login-ui.js
@@ -1,6 +1,6 @@
 // This file contains the sign-in authentication UI provided by Firebase.
 // Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
+//var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function(authResult, redirectUrl) {
@@ -12,9 +12,21 @@ var uiConfig = {
     uiShown: function() {
       // The widget is rendered.
       // Hide the loader.
-      document.getElementById('loader').style.display = 'none';
+     document.getElementById('loader').style.display = 'none'; 
     }
   },
+  //Start it here 
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+  //End it here 
+  signInFlow: 'popup',
+  signInSuccessUrl: '<url-to-redirect-to-on-success>',
+  signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
   signInSuccessUrl: './index.html',
@@ -24,5 +36,6 @@ var uiConfig = {
   ],
 };
 // The start method will wait until the DOM is loaded.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 ui.start('#firebaseui-auth-container', uiConfig);
 
